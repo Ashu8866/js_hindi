3ea26444e0bcb1652c8297fc428fa460df8d40bb
Ashutosh yadav <87809907+Ashu8866@users.noreply.github.com>
2024-07-18 12:20:57 +0000

    datatypes

---
+++ 01_Jsbasics/01_variable.js
@@ -0,0 +1,16 @@
+const accountID = 144235
+let accountEmail = "yaashutosh67@gmail.com"
+var accountPassword = "12345"
+accontCity = "jaipur"
+let accountState;
+
+//  accountID = 2
+/*
+prefer not
+*/
+accountEmail = "hc@123.com"
+accountPassword = "212121"
+accontCity = "Allahabad"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accontCity,accountState])
---
+++ 01_Jsbasics/02_datatypes.js
@@ -0,0 +1,19 @@
+ "use strict"  //treat all js code as new version
+
+// alert (3+3)  // we are using nodejs, not browser
+
+console.log(3+3) //code readablity should be high
+ console.log("Hitesh")
+
+ let name = "Ashutosh"
+ let age = 18
+ let isLoggedIn = false
+ let state = null
+
+ //number => 2 to power 53
+// bigint 
+//boolean => true/false
+// null => standalone value
+//object
+
+console.log(typeof "hitesh")
