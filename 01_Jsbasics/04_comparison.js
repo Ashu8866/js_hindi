fb1c960f819b5ede02d279d582617669cdc2dcc6
Ashutosh yadav <87809907+Ashu8866@users.noreply.github.com>
2024-07-20 10:19:03 +0000

    datatypes

--- 01_Jsbasics/04_comparison.js
+++ 01_Jsbasics/04_comparison.js
@@ -5,4 +5,8 @@
 
 console.log("2">1);
 console.log(null>=0);
-console.log("2"== 2);
+console.log("2"== 2);let myObj = {
+    name: "hitrsh",
+    age: 22,
+};
+
---
+++ 01_Jsbasics/05_datatypes.js
@@ -0,0 +1,25 @@
+//primitive
+
+// 7 types : string
+
+const score = 100
+const scoreValue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id===anotherid);
+//  const bigNumber = 555445566554n
+
+
+// array , objects, functions
+const heros = ["shaktiman", "naagraj", "dog"];
+const myFunction = function(){
+    console.log("helloworld");
+}
+
+console.log(typeof bigNumber)
